fb44f82250ee59c62bb570aa5b6eb6168927da1d
Breno Henrique <breno.hs@outlook.com>
2024-03-07 11:55:04 -0300

    Adicionando postagem de fotos

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
-import useForm from '../Hooks/useForm';
+import useForm from '../../Hooks/useForm';
 import { USER_POST } from '../../api';
 import { UserContext } from '../../UserContext';
-import useFetch from '../Hooks/useFetch';
+import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
 
 const LoginCreate = () => {
