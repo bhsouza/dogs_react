d971f6db84c0aaae7d05d856f49f25bf8d7e8883
Breno Henrique <breno.hs@outlook.com>
2024-03-12 18:47:11 -0300

    Adicionando mudança do head das paginas ao navegar

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Head from '../Helper/Head';
 
 const UserStats = () => {
-  return <div>Estatísticas</div>;
+  return (
+    <div>
+      <Head title="Estatísticas" description="Página de estatísticas do site dogs." />
+      Estatísticas
+    </div>
+  );
 };
 
 export default UserStats;
